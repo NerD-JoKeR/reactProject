18b3440dcb763e6424c39644b4ee5075892f52cf
NerD-JoKeR <kanjar_95@mail.ru>
2019-05-19 15:49:02 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+/* Components */
+import { Calculator } from './components/Calculator'
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <body>
+        <Calculator/>
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Calculator/Calculator.jsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+
+export class Calculator extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {}
+    }
+
+    render(){
+        return (
+            <div>
+                <button>
+                    Click Me
+                </button>
+            </div>
+        );
+    }
+
+}
+
